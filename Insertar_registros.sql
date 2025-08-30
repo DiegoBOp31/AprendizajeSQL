e4004f12f1549248732d0b87c80c3170b01623c1
Diego Rosales Rebollar <diegorila-31@hotmail.com>
2025-08-29 23:28:38 -0600

    Crear registros

---
+++ Insertar_registros.sql
@@ -0,0 +1,13 @@
+USE jugos;
+
+SELECT * FROM tbproductos;
+
+INSERT INTO tbproductos(
+producto, nombre, envase, volumen, sabor, precio
+) VALUES ('695594', 'Festival de sabores','botella pet', '1.5 litros', 'Asaí',18.51);
+INSERT INTO tbproductos(
+producto, nombre, envase, volumen, sabor, precio
+) VALUES ('1041119', 'Línea Citrus','botella vidrio', '700 ml', 'Lima',4.90);
+INSERT INTO tbproductos(
+producto, nombre, envase, volumen, sabor, precio
+) VALUES ('8128292', 'clean','lata', '350 ml', 'naranja',2.81);
