9e545e25e0958962e1cdfc8fd20a9ffd1e08ffca
Diego Rosales Rebollar <diegorila-31@hotmail.com>
2025-08-29 23:29:51 -0600

    Crear registros en nueva tabla

---
+++ Insertar_registros_tablanueva.sql
@@ -0,0 +1,10 @@
+USE jugos;
+
+SELECT * FROM tabla_de_vendedores;
+
+INSERT INTO tabla_de_vendedores(
+MATRICULA, NOMBRE, PORCENTAJE_COMISION
+) VALUES ('00235', 'Márcio Almeida Silva',8);
+INSERT INTO tabla_de_vendedores(
+MATRICULA, NOMBRE, PORCENTAJE_COMISION
+) VALUES ('00236', 'Cláudia Morais',8);
