407ebafc2e8b4ccb6288af2957831d48238cab92
Diego Rosales Rebollar <diegorila-31@hotmail.com>
2025-08-29 23:32:01 -0600

    Eliminamos registros

---
+++ Eliminamos_registros.sql
@@ -0,0 +1,6 @@
+SELECT * FROM tabla_de_vendedores;
+
+-- El comando DELETE solo sirve para eliminar registros 
+DELETE FROM tbproductos WHERE producto = '773912';
+
+DELETE FROM tabla_de_vendedores WHERE NOMBRE = 'Joan Geraldo de la Fonseca Junior';
