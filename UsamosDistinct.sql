5f24f9a90cbc04d828f97831b91a9a73fa28c990
Diego Rosales Rebollar <diegorila-31@hotmail.com>
2025-09-01 22:23:44 -0600

    Aprendemos a usar el comando DISTINCT

---
+++ UsamosDistinct.sql
@@ -0,0 +1,13 @@
+USE jugos_ventas;
+
+SELECT ENVASE, TAMANO FROM tabla_de_productos;
+
+SELECT DISTINCT ENVASE, TAMANO FROM tabla_de_productos;
+
+SELECT DISTINCT ENVASE, TAMANO, SABOR FROM tabla_de_productos
+WHERE SABOR = 'Naranja';
+
+-- DISTINCT sirve para filtrar registros duplicados, es decir, 
+-- si hay registros con la misma información en todos sus campos, 
+-- cuando usamos distinct sólo obtenemos una sola vez ese registro
+-- y no todas las veces que se repite
