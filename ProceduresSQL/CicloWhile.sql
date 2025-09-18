1f1d22451130a6390533a5ccfff27505e5185120
Diego Rosales Rebollar <diegorila-31@hotmail.com>
2025-09-17 21:42:04 -0600

    Pongo el procedure en el script

--- ProceduresSQL/CicloWhile.sql
+++ ProceduresSQL/CicloWhile.sql
@@ -2,3 +2,18 @@ CREATE TABLE tb_looping(ID INT);
 SELECT * FROM tb_looping;
 
 CALL looping(1,10);
+
+/*
+CREATE DEFINER=`root`@`localhost` PROCEDURE `looping`(vinicial INT, vfinal INT)
+BEGIN
+DECLARE vcontador INT;
+SET vcontador = vinicial;
+DELETE FROM tb_looping;
+WHILE vcontador <= vfinal
+DO
+INSERT INTO tb_looping VALUES (vcontador);
+SET vcontador = vcontador + 1;
+END WHILE;
+SELECT * FROM tb_looping;
+END
+*/
