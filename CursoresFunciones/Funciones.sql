aab3745da45667b763892178fe1ef9b25397fbe4
Diego Rosales Rebollar <diegorila-31@hotmail.com>
2025-09-20 16:48:41 -0600

    Iniciamos el proyecto creando las tablas

--- CursoresFunciones/Funciones.sql
+++ CursoresFunciones/Funciones.sql
@@ -4,8 +4,7 @@ SET GLOBAL log_bin_trust_function_creators = 1;
 SELECT f_define_sabor('Maracuyá');
 
 SELECT NOMBRE_DEL_PRODUCTO, SABOR, f_define_sabor(SABOR) 
-AS tipo
-FROM tabla_de_productos;
+AS tipo FROM tabla_de_productos;
 
 SELECT NOMBRE_DEL_PRODUCTO, SABOR FROM tabla_de_productos
 WHERE f_define_sabor(SABOR) = 'MUY RICO';
