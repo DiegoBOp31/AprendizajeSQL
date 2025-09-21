b87eadf23b059d96eb5dcf79db357a9f31862076
Diego Rosales Rebollar <diegorila-31@hotmail.com>
2025-09-21 16:32:23 -0600

    Creamos un stored procedure que genera ventas (facturas e items) automáticamente

---
+++ ProyectoEmpresa/StoredProcedureVentas.sql
@@ -0,0 +1,70 @@
+SELECT f_cliente_aleatorio() AS CLIENTE,
+f_producto_aleatorio() AS PRODUCTO,
+f_vendedor_aleatorio() AS VENDEDOR;
+
+SELECT MAX(NUMERO) FROM FACTURAS;
+
+CALL sp_venta('2025-06-15',3,100);
+/* Éste es el stored procedure
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_venta`(fecha DATE, maxitems INT, maxcantidad INT)
+BEGIN
+DECLARE vcliente VARCHAR(11);
+DECLARE vproducto VARCHAR(10);
+DECLARE vvendedor VARCHAR(5);
+DECLARE vcantidad, vitems, vnfactura INT;
+DECLARE vprecio FLOAT;
+DECLARE vcontador INT DEFAULT 1;
+
+SELECT MAX(NUMERO) + 1 INTO vnfactura FROM facturas;
+SET vcliente = f_cliente_aleatorio();
+SET vvendedor = f_vendedor_aleatorio();
+
+INSERT INTO facturas (NUMERO,FECHA,DNI,MATRICULA,IMPUESTO) 
+VALUES (vnfactura, fecha,vcliente,vvendedor,0.16);
+
+SET vitems = f_aleatorio(1,maxitems);
+WHILE vcontador <= vitems
+DO
+SET vproducto= f_producto_aleatorio();
+SET vcantidad = f_aleatorio(1,maxcantidad);
+SELECT PRECIO INTO vprecio FROM productos WHERE CODIGO = vproducto;
+
+INSERT INTO items(NUMERO, CODIGO,CANTIDAD,PRECIO) 
+VALUES (vnfactura,vproducto,vcantidad,vprecio);
+
+SET vcontador = vcontador + 1;
+END WHILE;
+END
+*/
+
+-- Volvemos a crear las tablas y registros de facturas e items
+CREATE TABLE facturas(
+NUMERO INT NOT NULL,
+FECHA DATE,
+DNI VARCHAR(11) NOT NULL,
+MATRICULA VARCHAR(5) NOT NULL,
+IMPUESTO FLOAT,
+
+PRIMARY KEY(NUMERO),
+FOREIGN KEY(DNI) REFERENCES clientes (DNI),
+FOREIGN KEY(MATRICULA) REFERENCES vendedores(MATRICULA)
+);
+
+CREATE TABLE items(
+NUMERO INT NOT NULL,
+CODIGO VARCHAR(10) NOT NULL,
+CANTIDAD INT,
+PRECIO FLOAT,
+
+PRIMARY KEY(NUMERO,CODIGO),
+FOREIGN KEY (NUMERO) REFERENCES facturas(NUMERO),
+FOREIGN KEY (CODIGO) REFERENCES productos(CODIGO)
+);
+
+INSERT INTO facturas
+SELECT NUMERO, FECHA_VENTA AS FECHA, DNI, MATRICULA, IMPUESTO 
+FROM jugos_ventas.facturas;
+
+INSERT INTO items
+SELECT NUMERO, CODIGO_DEL_PRODUCTO AS CODIGO, CANTIDAD, PRECIO 
+FROM jugos_ventas.items_facturas;
