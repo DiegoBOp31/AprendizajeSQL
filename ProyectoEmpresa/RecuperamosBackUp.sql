e564324863ecd59c0ef2947fb0535b83998c87f0
Diego Rosales Rebollar <diegorila-31@hotmail.com>
2025-09-22 16:21:21 -0600

    Agrego la línea de la ruta

--- ProyectoEmpresa/RecuperamosBackUp.sql
+++ ProyectoEmpresa/RecuperamosBackUp.sql
@@ -3,6 +3,8 @@
 CREATE DATABASE jugos_ventas;
 
 /*DESDE CMD
+También tenemos que entrar a esta ruta:
+C:\Program Files\MySQL\MySQL Server 8.0\bin>
 La clave está que ahora usamos el "<" en el comando antes de la ruta
 mysql -uroot -p < c:\Users/usuario/OneDrive/Escritorio\jugos_ventas_full.sql
 */
