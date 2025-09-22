1233b14cb4de0a432caef9e284218b6e585c522c
Diego Rosales Rebollar <diegorila-31@hotmail.com>
2025-09-22 14:18:55 -0600

    Este archivo contiene información de configuración

---
+++ ProyectoEmpresa/AdministracionDB.sql
@@ -0,0 +1,23 @@
+/*En esta ruta se encuentra la configuración de las conexiones que tengamos
+de nuestra base de datos, el archivo se llama connections.xml*/
+-- C:\Users\{tu usuario}{\AppData\Roaming\MySQL\Workbench
+
+/*
+MySQL se ejcuta en automático cuando arrancamos windows porque así lo 
+configuramos en la instalación, para detenerlo debemos ir a los servicios
+de windows. Buscarlo como MySQL80 y detenerlo.
+
+los comandos desde la cmd son los siguientes
+Detenerlo: net stop mysql80
+Iniciarlo: net start mysql80
+*/
+
+
+SHOW GLOBAL STATUS LIKE 'Created_tmp_disk_tables';
+SHOW GLOBAL STATUS LIKE 'Created_tmp_tables';
+SHOW GLOBAL VARIABLES LIKE 'tmp_table_size';
+-- Aumentamos el tamaño de creacion de tablas temporales
+-- SET GLOBAL tmp_table_size = 134603008;
+/*esta es la ruta del archivo my.ini de las variables de ambiente
+C:\ProgramData\MySQL\MySQL Server 8.0
+*/
