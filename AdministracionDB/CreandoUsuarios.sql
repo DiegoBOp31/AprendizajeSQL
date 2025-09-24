d4c03fe48f94eb2bfa9ea6d7e958f9ebec2d468a
Diego Rosales Rebollar <diegorila-31@hotmail.com>
2025-09-24 12:08:44 -0600

    Creamos usuarios de administrador, usuario, lectura y back up

---
+++ AdministracionDB/CreandoUsuarios.sql
@@ -0,0 +1,22 @@
+-- Creamos un nuevo usuario administrador con comandos
+CREATE USER 'admin02'@'localhost' IDENTIFIED BY 'admin02';
+-- Le damos todos los privilegios al usuario nuevo
+GRANT ALL PRIVILEGES ON *.* TO 'admin02'@'localhost' WITH GRANT OPTION;
+
+-- Creamos un nuevo usuario con comandos
+CREATE USER 'user02'@'localhost' IDENTIFIED BY 'user02';
+-- Le damos los privilegios al usuario nuevo
+GRANT SELECT, INSERT, UPDATE, DELETE, 
+CREATE TEMPORARY TABLES, LOCK TABLES, EXECUTE
+ ON *.* TO 'user02'@'localhost';
+ 
+ -- Creamos un nuevo usuario de lectura con comandos
+CREATE USER 'read01'@'localhost' IDENTIFIED BY 'read01';
+-- Le damos los privilegios al usuario nuevo
+GRANT SELECT, EXECUTE ON *.* TO 'read01'@'localhost';
+
+ -- Creamos un nuevo usuario de back-up con comandos
+CREATE USER 'back01'@'localhost' IDENTIFIED BY 'back01';
+-- Le damos los privilegios al usuario nuevo
+GRANT SELECT, RELOAD, LOCK TABLES, REPLICATION CLIENT 
+ON *.* TO 'back01'@'localhost';
